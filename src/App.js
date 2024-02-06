b7bfdac3930200ca0cbe65515b060d78469b7365
breddy053 <praveem.bheemreddy2023@gmail.com>
2024-02-06 15:38:32 +0000

    resolved app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 //import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Summerize from "./Components/Summerize";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="App">
-//         {/* <div
-//         className="header "
-//         style={{
-//           justifyContent: "space-between",
-//         }}>
-//         <div style={{ marginLeft: "-30px" }}>Automated meeting summerizer</div>
-//         <div>
-//           <img className="logo" src={logo3}></img>
-//         </div>
-//       </div> */}
-//         <Header />
-//         {/* <div className="content">
-//         <UploadSection />
-//       </div> */}
-//       </div>
-//       <Routes>
-//         <Route path="/summerize" component={Summerize} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Summerize from "./Components/Summerize";
 
 
 const App =()=>{
